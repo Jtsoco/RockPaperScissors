18feae15cfb8e93b5ab9a337bd6d81d78a5f6338
Jackson <jtsoco@gmail.com>
2022-11-07 17:22:46 +0900

    add multiple rounds

--- jscript.js
+++ jscript.js
@@ -24,9 +24,9 @@ function playerSelection () {
    let playerChoice = prompt ("Please enter Rock, Paper, or Scissors.");
    return (playerChoice);
 }
-function game() {
-    let y = (playerSelection ()).toLowerCase();
-    let x = (getComputerChoice()).toLowerCase();
+function playRound(y, x) {
+     y = (playerSelection ()).toLowerCase();
+     x = (getComputerChoice()).toLowerCase();
     if ( x === y) {
         return "It's a tie!"
     }
@@ -38,21 +38,52 @@ function game() {
     ((y === "paper") && (x === "scissors")) 
     || ((y === "scissors") && (x === "rock"))) {
         
-        return "You Lose!" + " " + x.charAt(0).toUpperCase() + x.slice(1) + " beats " + y.charAt(0).toUpperCase() + y.slice(1);
+        return "You Lose!" + " " + x.charAt(0).toUpperCase() + x.slice(1) + " beats " + y.charAt(0).toUpperCase() + y.slice(1) + ".";
     }
     else if (((y === "paper") && (x === "rock")) 
     || 
     ((y === "scissors") && (x === "paper")) 
     || ((y === "rock") && (x === "scissors"))) {
         y.charAt(0) + y.split(1);
-        return "You Win!" + " " + y.charAt(0).toUpperCase() + y.slice(1) + "beats " + x.charAt(0).toUpperCase() + x.slice(1); 
+        return "You Win!" + " " + y.charAt(0).toUpperCase() + y.slice(1) + " beats " + x.charAt(0).toUpperCase() + x.slice(1) + "."; 
     }
 
     else {
-        return "Ah E gad"
+        return "Ah oh no! You entered something wrong. Let's call this a loss!"
     }
-
-    
-
 }
+    function game() {
+        for (let i = 0; i <=5; i++){
+            switch (i) {
+                case 0:
+                    a = playRound();
+                    console.log (a)
+                break;
 
+                case 1:
+                    b = playRound();
+                    console.log(b);
+                    break;
+                case 2:
+                    c = playRound();
+                    console.log(c);
+                    break;
+                case 3:
+                    d = playRound();
+                    console.log(d);
+                    break;
+                case 4:
+                    e = playRound();
+                    console.log(e);
+                break;
+
+            }
+            }
+            return "Round 1 results: " + a + 
+            " Round 2 results: " + b +
+            " Round 1 results: " + c +
+            " Round 1 results: " + d + 
+            " Round 1 results: " + e;
+        }
+   
+    
